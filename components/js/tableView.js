eb3d4c5eab7f95aa3f2c374adfe798b7d0b2f733
urah_meshiya <ponzu946@gmail.com>
2026-04-01 16:21:30 +0900

    テーブル縦幅修正

--- components/js/tableView.js
+++ components/js/tableView.js
@@ -13,7 +13,7 @@ export class TableView {
     this.thead = this.dom.tableContainer.querySelector("thead");
     this.tbody = this.dom.tableContainer.querySelector("tbody");
 
-    this.dom.tableContainer.style.minHeight = `${window.innerHeight * 0.70}px`;
+    this.dom.tableContainer.style.minHeight = `${window.innerHeight * 0.65}px`;
   }
 
   render(header, data) {
