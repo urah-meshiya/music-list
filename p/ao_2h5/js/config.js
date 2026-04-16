f1a7302cd86483b4e456492792223b5fe6b2a421
urah_meshiya <ponzu946@gmail.com>
2026-04-16 13:15:59 +0900

    add ao_2h5

--- p/ao_2h5/js/config.js
+++ p/ao_2h5/js/config.js
@@ -1,21 +1,30 @@
 export const CONFIG = {
   /** Googleスプレッドシート(公開)のURL */
-  url: "https://docs.google.com/spreadsheets/d/xxxxxxxxxxxxxx/export?format=csv&gid=xxxxxxxxxxxxxx",
+  url: "https://docs.google.com/spreadsheets/d/1qEAgchkcRZ2Wqxpd0YMfjHbQnqDBYufwK9swauBEae0/export?format=csv&gid=815885868",
   /** ヘッダー行 */
   headerRowIndex: 2,
   /** 表示対象列 */
-  displayColumns: ["C", "D", "E", "F"],
+  displayColumns: ["D", "E"],
   /** 列番号(ソート目的も含む) */
   columnIndex: { A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6, H: 7, I: 8, J:9},
-  primaryCol: "C",
-  
+  primaryCol: "D",
+  secondaryCol: "E",
+
   /************ カスタマイズ項目 ************/
   /** ページタイトル */
-  title: "XXXの歌える曲リスト",
+  title: "Music List",
   /** ソートタブ(対象列,表示名) */
-  sortTab: { H:"曲名順", I:"作者順", F:"年代順", J:"ｵｽｽﾒ順"},
+  sortTab: { H:"曲名順", I:"作者順"},
   /** 検索対象 */
   searchTargets: { 0:{col:"H", type:"曲名", icon:"🎵", appendCol:"D"}, 1:{col:"I", type:"作者名", icon:"👤", appendCol:"E"}},
   /** 検索種類 */
-  matchingConditions : "include"
+  matchingConditions : "include",
+
+  /** テーブル拡大率 */
+  tableHeightRatio: 0.8,
+
+  /** 情報アイコンを出す列 */
+  infoIconCol: "D",
+  /** 情報アイコンホバー時の情報ソース列 */
+  infoSrcCol: "G"
 };
