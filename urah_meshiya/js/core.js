33b00944570cb028b19b3f5da9554057d896f959
urah_meshiya <ponzu946@gmail.com>
2026-04-20 17:02:56 +0900

    add test

--- urah_meshiya/js/core.js
+++ urah_meshiya/js/core.js
@@ -44,3 +44,42 @@ th.addEventListener("click", (e) => {
 });
 
 document.addEventListener("click", hide);
+
+const testBtn = app.dom.tabArea.querySelector("#testBtn");
+
+// テスト
+testBtn.addEventListener("click", async () => {
+  try {
+    const res = await fetch("https://fragrant-king-6465.ponzu946.workers.dev/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": "a8F3kL9xQ2pZ7wR6mN4sT1vB0yC"
+      },
+      body: JSON.stringify({
+        userId: "urah_meshiya",
+        comment: "Happy"
+      })
+    });
+
+    if (!res.ok) {
+      const text = await res.text();
+      console.error("HTTPエラー:", res.status, text);
+      return;
+    }
+
+    let data;
+    try {
+      data = await res.json();
+    } catch {
+      const text = await res.text();
+      console.warn("JSONでないレスポンス:", text);
+      return;
+    }
+
+    console.log("成功:", data);
+
+  } catch (err) {
+    console.error("通信エラー:", err);
+  }
+});
