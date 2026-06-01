4c7967b32108b554c263151c5416d5f1c98f9005
urah_meshiya <ponzu946@gmail.com>
2026-06-01 10:50:51 +0900

    add teliiru

---
+++ p/teliiru/js/config.js
@@ -0,0 +1,33 @@
+export const CONFIG = {
+  /** Googleスプレッドシート(公開)のURL */
+  url: "https://docs.google.com/spreadsheets/d/1DcrplaiwuEOXP58lmgDptD8uCu85zzltztLKT4kmFTY/export?format=csv&gid=815885868",
+  
+  /** ヘッダー行 */
+  headerRowIndex: 2,
+  /** 表示対象列 */
+  displayColumns: ["C", "D", "E", "F"],
+  primaryCol: "D",
+  secondaryCol: "E",
+  /** 列番号(ソート目的も含む) */
+  columnIndex: { A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6, H: 7, I: 8, J:9, K:10},
+
+  /************ カスタマイズ項目 ************/
+  /** ページタイトル */
+  title: "てぃーるさんの歌える曲リスト",
+  /** ソートタブ(対象列,表示名) */
+  sortTab: { I:"曲名順", J:"作者順", F:"年代順", C:"ｵｽｽﾒ順"},
+  /** 検索対象 */
+  searchTargets: { 0:{col:"I", type:"曲名", icon:"🎵", appendCol:"D"}, 1:{col:"J", type:"作者名", icon:"👤", appendCol:"E"}},
+  /** 検索種類 */
+  matchingConditions : "include",
+  /** テーブル拡大率 */
+  tableHeightRatio: 0.88,
+  /** 情報アイコンを出す列 */
+  infoIconCol: "D",
+  /** 情報アイコンホバー時の情報ソース列 */
+  infoSrcCol: "H",
+  /** URL列 */
+  urlSrcCol: "G",
+  /** URL表示列 */
+  urlTargetCol: "C"
+};
---
+++ p/teliiru/js/core.js
@@ -0,0 +1,46 @@
+import { CONFIG } from "./config.js";
+import { App } from "../../../components/js/app.js";
+
+const app = new App(CONFIG);
+await app.completed;
+
+// ユーザ毎のカスタムJS
+const th = app.dom.tableContainer.querySelector("thead th:first-child");
+const tooltip = document.querySelector("#tooltip");
+const DESCRIPTION = "＜オススメ度＞\n×＜▲＜ - ＜○＜◎＜★";
+
+let isOpen = false;
+
+function show(x, y) {
+  tooltip.textContent = DESCRIPTION;
+  tooltip.classList.add("visible");
+  const offset = 8;
+  tooltip.style.left = x + offset + "px";
+  tooltip.style.top = y - tooltip.offsetHeight - offset + "px";
+
+  isOpen = true;
+}
+
+function hide() {
+  tooltip.classList.remove("visible");
+  isOpen = false;
+}
+
+th.addEventListener("mouseenter", () => {
+  tooltip.textContent = DESCRIPTION;
+  tooltip.classList.add("visible");
+});
+
+th.addEventListener("mousemove", (e) => {
+  show(e.clientX, e.clientY);
+});
+
+th.addEventListener("mouseleave", hide);
+
+th.addEventListener("click", (e) => {
+  e.stopPropagation();
+  const rect = th.getBoundingClientRect();
+  show(rect.right, rect.top);
+});
+
+document.addEventListener("click", hide);
